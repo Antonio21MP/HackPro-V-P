c296c82fb332a2dd7fc946740d43782c5beb50bd
José Antonio <jose_mejiap34@unitec.edu>
2015-12-03 17:17:25 -0600

    Primer Avance Grafico

---
+++ HackPro2/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HackPro2.Models;
+
+
+namespace HackPro2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel log)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+    }
+}
